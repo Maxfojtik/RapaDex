4ce42760bf440a4806be64ab51ebebad40b7d50b
Maxfojtik <maxfojtik@bex.net>
2021-09-29 18:01:23 -0400

    added front end

--- my-electron-app/entry.js
+++ my-electron-app/entry.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow } = require('electron')
 function createWindow () 
 {
-	const win = new BrowserWindow({width: 800, height: 600, autoHideMenuBar: true})
-	win.loadFile('index.html')
+	const win = new BrowserWindow({width: 1600, height: 900, autoHideMenuBar: true});
+	win.loadFile('index.html');
 }
   app.whenReady().then(() => {  createWindow()})
