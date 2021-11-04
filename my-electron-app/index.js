9d9f08241e33abef4ef24e3c8c5a0d90f9b21cf1
Maxfojtik <maxfojtik@bex.net>
2021-11-04 15:24:56 -0400

    Added Configuration

--- my-electron-app/index.js
+++ my-electron-app/index.js
@@ -22,6 +22,16 @@ $(document).on('click', '.copiable', function () {
 	$('#toastText').text("Copied \""+text+"\" to clipboard");
 	new bootstrap.Toast($('#liveToast')).show();
 });
+function findPerson()
+{
+	//alert('hi');
+	/*var xhr = new XMLHttpRequest();
+	xhr.open("POST", yourUrl, true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify({
+		value: value
+	}));*/
+}
 function closeSaveAsPopover()
 {
 	var popover = bootstrap.Popover.getInstance($('#saveAsButton'));
@@ -30,13 +40,67 @@ function closeSaveAsPopover()
 		popover.hide();
 	}
 }
+var darkTexts = ['Chloe', 'Brennan', 'Dave'];
 function markSelectedPill(thePill, theName)
 {
-	thePill.className = 'badge rounded-pill badge-'+theName+' badge-spaced';
+	if(darkTexts.indexOf(theName)!=-1)
+	{
+		thePill.className = 'badge rounded-pill badge-'+theName+' badge-spaced text-dark';
+	}
+	else
+	{
+		thePill.className = 'badge rounded-pill badge-'+theName+' badge-spaced';
+	}
 }
 function markDeselectedPill(thePill, theName)
 {
 	thePill.className = 'badge rounded-pill badge-'+theName+'-ns badge-not-selected text-dark badge-spaced';
+}
+function makeSelect(name)
+{
+	var allMakes = $("#makeSelector").children();
+	var theMake;
+	for(var i = 0; i < allMakes.length; i++)
+	{
+		var theName = allMakes[i].innerHTML;
+		allMakes[i].className = 'btn btn-outline-primary';
+		if(theName==name)
+		{
+			theMake = allMakes[i];
+		}
+	}
+	if(theMake.innerHTML=='Other')
+	{
+		$("#makeOtherBox").show();
+	}
+	else
+	{
+		$("#makeOtherBox").hide();
+	}
+	theMake.className = 'btn btn-primary';
+}
+function typeSelect(id)
+{
+	var allTypes = $("#typeSelectors").find('button');
+	var theType;
+	for(var i = 0; i < allTypes.length; i++)
+	{
+		var theName = allTypes[i].id;
+		allTypes[i].className = 'btn btn-outline-primary';
+		if(theName==id)
+		{
+			theType = allTypes[i];
+		}
+	}
+	if(theType.innerHTML=='Other')
+	{
+		$("#typeOtherBox").show();
+	}
+	else
+	{
+		$("#typeOtherBox").hide();
+	}
+	theType.className = 'btn btn-primary';
 }
 function selectPill(name)
 {
